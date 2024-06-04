f7ca9fe7a606b933753256379a31c6c160a579f8
Etai Levi <etai3000@gmail.com>
2024-06-04 23:19:51 +0300

    UPDATE: Added pagination to movie-index

--- src/views/movie-index.jsx
+++ src/views/movie-index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { movieService } from '../services/movie.service'
 import { MovieList } from '../cmps/movie-list'
 import { MovieFilter } from '../cmps/movie-filter'
@@ -10,15 +10,17 @@ export function MovieIndex() {
 
     const [error, setError] = useState()
     const [isLoading, setIsLodaing] = useState(false)
+    const [page, setPage] = useState(1)
     const [movies, setMovies] = useState(movieService.query() || [])
     const [filterBy, setFilterBy] = useState(movieService.getDefaultFilter())
     const [sortBy, setSortBy] = useState()
     // const API_KEY = process.env.MOVIES_API_KEY
+    const API_KEY = 'bd51ecc90bdea3ebcacaad222ab4a555'
     useEffect(() => {
         async function fetchMovies() {
             setIsLodaing(true)
             try {
-                const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=bd51ecc90bdea3ebcacaad222ab4a555`)
+                const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}&api_key=${API_KEY}`)
                 const data = await response.json()
                 movieService.saveMovies(data.results)
                 const filteredMovies = movieService.query(filterBy)
@@ -37,7 +39,7 @@ export function MovieIndex() {
 
         // fetchMoviesWithDelay()
         fetchMovies()
-    }, [filterBy, sortBy])
+    }, [filterBy, sortBy, page])
 
     function onSetFilter(filterBy) {
         setFilterBy(prevFilterBy => ({ ...prevFilterBy, ...filterBy }))
@@ -62,6 +64,14 @@ export function MovieIndex() {
         }
     }
 
+    function setPrevPage() {
+        if (page > 1) setPage(prev => prev - 1)
+    }
+
+    function setNextPage() {
+        setPage(prev => prev + 1)
+    }
+
     return (
         <section className="movie-index">
             <div className="control-panel">
@@ -82,6 +92,11 @@ export function MovieIndex() {
                     </h3>
                 </div>
             }
-        </section >
+            <div className='page-section'>
+                <button onClick={setPrevPage} disabled={page <= 1}>-</button>
+                <span>{page}</span>
+                <button onClick={setNextPage}>+</button>
+            </div>
+        </section>
     )
 }
