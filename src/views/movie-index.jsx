92ac23b506cfbacc68c5d687f058d3afbf1025dd
Etai Levi <etai3000@gmail.com>
2024-06-04 21:05:32 +0300

    UPDATE: Added a loader to movie-index

--- src/views/movie-index.jsx
+++ src/views/movie-index.jsx
@@ -3,15 +3,20 @@ import { movieService } from '../services/movie.service'
 import { MovieList } from '../cmps/movie-list'
 import { MovieFilter } from '../cmps/movie-filter'
 import { MovieSort } from '../cmps/movie-sort'
+import { Link } from 'react-router-dom'
+import { Loader } from '../cmps/loader'
 
 export function MovieIndex() {
 
+    const [error, setError] = useState()
+    const [isLoading, setIsLodaing] = useState(false)
     const [movies, setMovies] = useState(movieService.query() || [])
     const [filterBy, setFilterBy] = useState(movieService.getDefaultFilter())
     const [sortBy, setSortBy] = useState()
     // const API_KEY = process.env.MOVIES_API_KEY
     useEffect(() => {
         async function fetchMovies() {
+            setIsLodaing(true)
             try {
                 const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=bd51ecc90bdea3ebcacaad222ab4a555`)
                 const data = await response.json()
@@ -20,9 +25,17 @@ export function MovieIndex() {
                 const sortedMovies = sortMovies(filteredMovies, sortBy)
                 setMovies(sortedMovies)
             } catch (err) {
-                console.error('Cannot load movies', err)
+                setError(err)
+            } finally {
+                setIsLodaing(false)
             }
         }
+
+        // function fetchMoviesWithDelay() {
+        //     setTimeout(fetchMovies, 2000)
+        // }
+
+        // fetchMoviesWithDelay()
         fetchMovies()
     }, [filterBy, sortBy])
 
@@ -55,13 +68,20 @@ export function MovieIndex() {
                 <MovieFilter onSetFilter={onSetFilter} filterBy={filterBy} />
                 <MovieSort onSetSort={onSetSort} />
             </div>
-            {movies.length > 0 ? (
-                <>
-                    <MovieList movies={movies} />
-                </>
-            ) : (
-                <h3>Loading Movies...</h3>
-            )}
-        </section>
+            {isLoading && <Loader />}
+            {!isLoading &&
+                <MovieList movies={movies} />
+            }
+            {error &&
+                <div className='error'>
+                    <h3>Something went wrong! Please try again
+                        <Link to='/movie'>
+                            <span>here</span>
+                        </Link>
+                        .
+                    </h3>
+                </div>
+            }
+        </section >
     )
 }
