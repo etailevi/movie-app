528ef9658b2435d331054560c2c308bf3d7f81af
Etai Levi <etai3000@gmail.com>
2024-06-04 21:06:02 +0300

    UPDATE: Design home-page

--- src/views/home-page.jsx
+++ src/views/home-page.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import CoverPhoto from '../assets/img/home-page-cover.avif'
 
 export function HomePage() {
     return (
-        <div>HomePage</div>
+        <div className='home-page'>
+            <div
+                className="cover-photo"
+                style={{ background: `url(${CoverPhoto}) center center / cover` }}
+            ></div>
+            <h1>Welcome to Movies App!</h1>
+            <h2>Looking for a good movie to watch with your beloved ones?</h2>
+            <button><Link to='/movie'>Click me!</Link></button>
+        </div>
     )
 }
